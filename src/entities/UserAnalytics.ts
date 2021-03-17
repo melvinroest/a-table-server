b7573e0d5ef30fa87ca1e6e777f88f139a235604
melvin roest <melvinroest@gmail.com>
2021-03-17 22:46:04 +0100

    simple cleanup

--- src/entities/UserAnalytics.ts
+++ src/entities/UserAnalytics.ts
@@ -9,37 +9,29 @@ export class UserAnalytics {
   source: string;
 
   @Column({type: 'real'})
-  // @Column()
   users: number;
 
   @Column({type: 'real'})
-  // @Column()
   new_users: number;
 
   @Column({type: 'real'})
-  // @Column()
   sessions: number;
 
   @Column({type: 'real'})
-  // @Column()
   bounce_rate: number;
 
   @Column()
   first_seen_on: Date;
 
   @Column({type: 'real'})
-  // @Column()
   pages_session: number;
 
   @Column({type: 'real'})
-  // @Column()
   avg_session_duration: number;
 
   @Column({type: 'real'})
-  // @Column()
   conversion_rate: number;
 
   @Column({type: 'real'})
-  // @Column()
   goal_value: number;
 }
